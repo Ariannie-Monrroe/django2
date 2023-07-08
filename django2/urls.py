05bf75133fa4cb32252fe41b7283bce499b51b51
Ariannie Monrroe <ariannie.monrroe@gmail.com>
2023-07-08 02:28:21 -0400

    Api

--- django2/urls.py
+++ django2/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('', include("apps.Tienda.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
